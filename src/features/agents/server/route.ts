3a46ed7af466672d962b2dada60c72a350b8b2ac
Nitin Saini <t92514920@gmail.com>
2026-06-28 21:07:32 +0530

    routes added for get and create agents

--- src/features/agents/server/route.ts
+++ src/features/agents/server/route.ts
@@ -1,18 +1,39 @@
 import { db } from "@/db";
 import { agents } from "@/db/schema";
 import { Hono } from "hono";
+import { auth } from "@/lib/auth"; 
 
-const app=new Hono()
-.get("/",async(c)=>{
-  console.log("hitting point")
-    const data={
-      name:"nitin"
-    }
+const app = new Hono()
+  .get("/", async (c) => {
+    const data = await db.select().from(agents);
     return c.json({
-        status:"200",
-        data:data
-    })
-})
+      status: 200,
+      data: data,
+    });
+  })
+  .post("/", async (c) => {
+    const body = await c.req.json();
 
+    const session = await auth.api.getSession({
+      headers: c.req.raw.headers,
+    });
 
-export default app
+    if (!session?.session?.userId) {
+      return c.json({ 
+        status: 401, 
+        error: "Unauthorized: Active session signature missing" 
+      }, 401);
+    }
+
+    const [newAgent] = await db.insert(agents).values({
+      userId: session.session.userId,
+      ...body,
+    }).returning(); 
+
+    return c.json({
+      status: 200,
+      data: newAgent,
+    });
+  });
+
+export default app;
